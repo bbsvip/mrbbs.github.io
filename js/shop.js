4f2055a6c7bfbcbe9ededd9f9d6d8b026be60690
Cừu Đen <39774913+bbsvip@users.noreply.github.com>
2021-04-11 21:01:03 +0700

    Add files via upload

---
+++ js/shop.js
@@ -0,0 +1,98 @@
+var total = 0;
+var dem = 0;
+function addItem1(){
+    var text = document.getElementById('money1').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem2(){
+    var text = document.getElementById('money2').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem3(){
+    var text = document.getElementById('money3').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem4(){
+    var text = document.getElementById('money4').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem5(){
+    var text = document.getElementById('money5').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem6(){
+    var text = document.getElementById('money6').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem7(){
+    var text = document.getElementById('money7').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem8(){
+    var text = document.getElementById('money8').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem9(){
+    var text = document.getElementById('money9').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem10(){
+    var text = document.getElementById('money10').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem11(){
+    var text = document.getElementById('money11').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
+function addItem12(){
+    var text = document.getElementById('money11').value;
+    var number = text.match(/\d/g);
+    number = number.join("");
+    total += parseFloat(number);
+    dem++;
+    document.getElementById('dem').innerHTML= dem;
+}
